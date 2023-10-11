741612965dbffd317a9839536f5902c141cb1bb8
Kooale325 <muhammad.bilal832017@gmail.com>
2023-10-11 22:25:56 +0500

    Initial commit

---
+++ Main3.cpp
@@ -0,0 +1,15 @@
+//#include<iostream>
+//#include<fstream>
+//using namespace std;
+//int main() {
+//	ofstream of; 
+//	of.open("BinaryArr.txt");
+//	int resx = 40;
+//	int resy = 90;
+//	for (int i = 0; i < resy; i++) {
+//		for (int j = 0; j < resx; j++) {
+//				of << 1 << ",";
+//		}
+//		of << 254<<",";
+//	}
+//}
